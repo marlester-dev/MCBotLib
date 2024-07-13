9c4059840c386c8646e1be4877df29a9d3ff2138
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-07-13 16:14:47 +0200

    License Apache 2.0

--- src/main/java/me/marlester/mcbotlib/Bot.java
+++ src/main/java/me/marlester/mcbotlib/Bot.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2023 Marlester
+ * Copyright (C) 2024 marlester-dev
  *
- * Licensed under the EUPL, Version 1.2 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You may not use this work except in compliance with the License.
- * You may obtain a copy of the License at:
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * https://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS" basis,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
- * and limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package me.marlester.mcbotlib;
